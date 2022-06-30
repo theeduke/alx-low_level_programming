b48877e84db41aacdfc6acaa2ed2999745956385
theeduke <oscarian528@gmail.com>
2022-06-30 07:38:54 -0700

    preprocessor

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,5 @@
 #!/bin/bash
 #include <stdio.h>
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-int main (){
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
+void main(){
+	printf("with proper grammar, but the outcome is a piece of art");
 }
