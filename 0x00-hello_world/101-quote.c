a450eada2deccf4c55d21961f6db6a21a96780a6
theeduke <oscarian528@gmail.com>
2022-06-30 15:08:48 -0700

    number 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description-not using printf nor puts
+ *
+ * Return:0 (success
+ */)
 int main(void)
-	{
-		write(""and that piece of art is useful" - Dora Korpar, 2015-10-19");
-		return ((0));
-	}
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
