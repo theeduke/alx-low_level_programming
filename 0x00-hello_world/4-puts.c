5479a24970168d2fcb8f04c2bfdb34de33d37fde
theeduke <oscarian528@gmail.com>
2022-06-30 08:49:22 -0700

    using put instead of printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,2 @@
 #!/bin/bash
-#include <stdio.h>
-int main(void){puts(printf("Programming is like building a multilingual puzzle"); return 0;}
+#include <stdio.h> int main(void){puts(printf("Programming is like building a multilingual puzzle"); return 0;}
