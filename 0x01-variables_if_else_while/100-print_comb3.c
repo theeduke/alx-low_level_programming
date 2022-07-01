73fb91c7cd46c6a70117aa6b40fd1c9da52f24ff
theeduke <oscarian528@gmail.com>
2022-07-01 15:29:33 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main-main entry
+ *
+ * return: 0 (success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		for (b = a + '1'; b <= '9'; b++)
+		{
+			if (b != a)
+			{
+				putchar(a);
+				putchar(b);
+
+				if (a == '8' && b == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+		return (0);
+	}
+}
