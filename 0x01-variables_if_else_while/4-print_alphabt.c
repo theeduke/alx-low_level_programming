4a8e991a0a8903c2cd479d87edab3efdd7af92ee
theeduke <oscarian528@gmail.com>
2022-07-01 10:36:08 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return:0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch='a';ch<='z';ch++)
+	{ 
+		if (ch != 'q' && ch != 'e' )
+		{
+			putchar(ch);
+		}
+	}
+	return(0);
+}
