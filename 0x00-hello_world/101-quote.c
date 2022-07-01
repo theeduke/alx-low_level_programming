19645a0863758b27f44b4651275b5fe782d9cebb
theeduke <oscarian528@gmail.com>
2022-07-01 09:06:21 -0700

    last one

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
  *
  * description-not using printf nor puts
  *
  * Return:0 (success
- */)
+ */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (0);
 }
