6d78638b8addc31bf8e696b1b3c6f6d949f6baf2
theeduke <oscarian528@gmail.com>
2022-07-01 13:35:28 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return:0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		printf("%d\n", a);
+	}
+	return (0);
+}
