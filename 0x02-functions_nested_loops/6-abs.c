25a1dd8f2adeaf018cb03b4019824fa299388a2a
theeduke <oscarian528@gmail.com>
2022-07-05 09:56:31 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Entry point
+ *
+ * int: input of the number
+ *
+ * Return: absolut value of r
+ */
+int _abs(int)
+{
+	int r;
+
+	if (r > 0)
+		return (r);
+	else
+		return (r * -1);
+}
+
