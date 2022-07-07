85257c0b10a51713be28e045ec787b26c3b1dd03
theeduke <oscarian528@gmail.com>
2022-07-07 16:32:19 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,33 +1,11 @@
-#ifndef  MAIN_H                                                                                                                         
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
-#define  MAIN_H                                                                                                                         
+int _putchar(char c);
 
-                                                                                                                                        
-
-int _putchar (char c);                                                                                                          
-
-int largest_number(int a, int b, int c);                                                                                 
-
-int _islower(int c);                                                                                                                    
-
-int _isalpha(int c);                                                                                                                    
-
-int print_sign(int n);                                                                                                                  
-
-int _abs(int);                                                                                                                          
-
-int print_last_digit(int);                                                                                                              
-
-void jack_bauer(void);                                                                                                                  
-
-void times_table(void);                                                                                                                 
-
-int add(int, int);                                                                                                                      
-
-void print_to_98(int n);                                                                                                                
-
-void print_times_table(int n);                                                                                                          
-
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-#endif    
+#endif
