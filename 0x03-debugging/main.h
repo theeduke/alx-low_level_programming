acde79abb656e3f075449c2d9470328d3063fe32
theeduke <oscarian528@gmail.com>
2022-07-07 13:14:48 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,11 +4,9 @@
 
                                                                                                                                         
 
-int _putchar (char c);                                                                                                                  
+int _putchar (char c);                                                                                                          
 
-void print_remaining_days(int month, int day, int year)                                                                                                              
-
-int largest_number(int a, int b, int c)                                                                                 
+int largest_number(int a, int b, int c);                                                                                 
 
 int _islower(int c);                                                                                                                    
 
@@ -29,5 +27,6 @@ int add(int, int);
 void print_to_98(int n);                                                                                                                
 
 void print_times_table(int n);                                                                                                          
+void print_remaining_days(int month, int day, int year);
 
 #endif    
