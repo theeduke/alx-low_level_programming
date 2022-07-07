8ee9f75e4b38dca5da4cedfcec721e3c96f193d8
theeduke <oscarian528@gmail.com>
2022-07-07 13:52:00 -0700

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,10 +3,12 @@
 #define  MAIN_H                                                                                                                         
 
                                                                                                                                         
+int _isupper(int c);
+                                                                                                                                        int _isdigit(int c);
+                    
+int mul(int a, int b);
 
-                                                                                                                                        
-
-                                                                                                                                        
+void print_numbers(void);
 
 int _putchar (char c);                                                                                                                  
 
@@ -30,7 +32,17 @@ int print_sign(int n);
 
 int _abs(int);                                                                                                                          
 
-                                                                                                                                        
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
 
 int print_last_digit(int);                                                                                                              
 
@@ -46,7 +58,7 @@ void times_table(void);
 
 int add(int, int);                                                                                                                      
 
-                                                                                                                                        
+                                                                                                                                      
 
 void print_to_98(int n);                                                                                                                
 
