b67752c820f3cb8c63d6ccc1f38a783390aa5e6b
theeduke <oscarian528@gmail.com>
2022-07-07 13:24:34 -0700

    task 3

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -27,6 +27,7 @@ int add(int, int);
 void print_to_98(int n);                                                                                                                
 
 void print_times_table(int n);                                                                                                          
+
 void print_remaining_days(int month, int day, int year);
 
 #endif    
