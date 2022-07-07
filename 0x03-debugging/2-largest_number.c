35bf60a08a1dfc63c851d09b292814fcbc389fe5
theeduke <oscarian528@gmail.com>
2022-07-07 13:03:44 -0700

    larhest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -6,21 +6,41 @@
  * @c: third integer
  * Return: largest number
  */
-int largest_number(int a, int b, int c)
-{
-    int largest;
 
-    if (a >= b && b => c)
-    {
-        largest = a;
-    }
-    else if (b >= a && a >= c)
-    {
-        largest = b;
-    }
-    else if (c >= a && a >= b)
-    {
-        largest = c;
-    }
-    return (largest);
+int largest_number(int a, int b, int c)
+
+{
+
+	int largest;
+
+
+
+	if (a >= b && a >= c)
+
+	{
+
+		largest = a;
+
+	}
+
+	else if (b >= a && b >= c)
+
+	{
+
+		largest = b;
+
+	}
+
+	else if (c >= a && c >= b)
+
+	{
+
+		largest = c;
+
+	}
+
+
+
+	return (largest);
+
 }
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,33 @@
+#ifndef  MAIN_H                                                                                                                         
+
+#define  MAIN_H                                                                                                                         
+
+                                                                                                                                        
+
+int _putchar (char c);                                                                                                                  
+
+void print_remaining_days(int month, int day, int year)                                                                                                              
+
+int largest_number(int a, int b, int c)                                                                                 
+
+int _islower(int c);                                                                                                                    
+
+int _isalpha(int c);                                                                                                                    
+
+int print_sign(int n);                                                                                                                  
+
+int _abs(int);                                                                                                                          
+
+int print_last_digit(int);                                                                                                              
+
+void jack_bauer(void);                                                                                                                  
+
+void times_table(void);                                                                                                                 
+
+int add(int, int);                                                                                                                      
+
+void print_to_98(int n);                                                                                                                
+
+void print_times_table(int n);                                                                                                          
+
+#endif    
