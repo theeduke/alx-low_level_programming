f1546ad67722e45067429dec528403cf3e675593
theeduke <oscarian528@gmail.com>
2022-07-07 11:23:22 -0700

    moer nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _isupper - display uppercase letters
+ * @c:Varible being sorted
+ *
+ * Return: 1 or 0 otherwise
+ */
+int _isupper(int c)
+{
+	int cha;
+
+	upper = 0;
+
+	for (cha == 'a'; cha <= 'z'; cha++)
+	{
+		for (cha == 'A'; cha <= 'Z'; cha++)
+		{
+			if (cha == 'C')
+				upper = 1;
+		}
+	}
+	return (upper);
+}
