69be4efa961bd98423313940618fe6b97868529b
theeduke <oscarian528@gmail.com>
2022-07-20 05:29:11 -0700

    task 5

--- 0x08-recursion/5-sqrt-recursion.c
+++ 0x08-recursion/5-sqrt-recursion.c
@@ -1,30 +1,34 @@
 #include "main.h"
-int _sqrt(int prev, int root);
-
 /**
- * _sqrt_recursion - It returns the value of square root of n.
- * @n: an input integer
- * Return: The square root of n
+ * _sqrt - descripcion
+ * @x: number
+ * @y: number
+ * Return: suma de valores
+ */
+int _sqrt(int x, int y)
+{
+	int sum;
+
+	sum = x - y;
+	if (sum < 0)
+		return (-(y / 2) - 1);
+	if (sum == 0)
+		return (1);
+	else
+		return (1 + _sqrt(sum, y + 2));
+}
+/**
+ * _sqrt_recursion - raiz cuadrada natural
+ * @n: number radicando
+ * Return: resultado
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-
-	return (_sqrt(1, n));
-}
-
-/**
- * _sqrt - find square root
- * @prev: previous value
- * @root: square root value
- * Return: the square root
- */
-int _sqrt(int prev, int root)
-{
-	if (prev > root)
-		return (-1);
-	else if (prev * prev == root)
-		return (prev);
-	return (_sqrt(prev + 1, root));
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (_sqrt(n, 1));
 }
