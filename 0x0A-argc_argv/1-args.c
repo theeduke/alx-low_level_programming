c238b5e610164c5be7b5d747e6016900b2e2f77b
theeduke <oscarian528@gmail.com>
2022-07-22 06:12:18 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -  Entry poing
+ * @argc: argument count
+ * @argv: argument value
+ * Return: 0 (success)
+ */
+int main(argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
