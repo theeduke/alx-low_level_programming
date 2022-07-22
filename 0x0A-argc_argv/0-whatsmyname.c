cac7de777af58a14f9d92b1cccc8f5b938273185
theeduke <oscarian528@gmail.com>
2022-07-22 05:53:38 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - function to print itself
+ * @argc: argument counter
+ * @argv: argument value
+ * Return: 0 (succes)
+ */
+int main(int argc, char *argv[])
+{
+	if (agrv[0])
+	{
+		printf("%d\n", argc - 1");
+	}
+	return (0);
+}
