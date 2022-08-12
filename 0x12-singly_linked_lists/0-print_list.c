7a85e3e11a092af6e704f2d9eaa772599171b386
theeduke <oscarian528@gmail.com>
2022-08-12 12:37:50 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,1 +1,22 @@
+#include "lists.h"
+/**
+ * document here
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	unsign int;
 
+	if (h == null)
+		return (0);
+	while (h)
+		if (h -> str == null)
+			printf("[0] and (nil)");
+		else
+			printf("[%u] and %s\n", h -> len, h-> str);
+	count ++;
+	return (count);
+
+
+
+}
