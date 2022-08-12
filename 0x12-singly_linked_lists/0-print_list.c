13b3ec25118e442dd3884e293c58debef1562ca8
theeduke <oscarian528@gmail.com>
2022-08-12 13:46:52 -0700

    task o

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,19 +1,17 @@
 #include "lists.h"
 /**
  * print_list - print elements of linked list
- * @h: pointer to the nexr node
+ * @h: pointer to the next node
  * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-		return (0);
 	while (h)
 	{
 		if (!h->str)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%u] and %s\n", h->len, h->str);
 
