07a0b705b629660c2013e48818a0d26ebfbf2e5c
theeduke <oscarian528@gmail.com>
2022-08-16 05:04:36 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -7,13 +7,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t nnodess = 0;
+	size_t count = 0;
 	
 	while (h != NULL)
 	{
 		h = h->next;
 		
-		nnodes++;
+		count++;
 	}
-	return (nnodes);
+	return (count);
 }
