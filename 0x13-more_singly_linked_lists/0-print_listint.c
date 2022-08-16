6ab8213681bb30b51db0bdda82f0de6e7f06ef7b
theeduke <oscarian528@gmail.com>
2022-08-16 02:45:39 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_listint - returns number of nodes
+ * @h: pointer to node head
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	count = 0;
+
+	if (h == NULL)
+		printf("Make sure head pointer is not null");
+	struct listint_t *ptr = NULL;
+
+	ptr = h;
+
+	while (ptr != NULL)
+	{
+		count++;
+
+		ptr = ptr->next
+	}
+	printf("%d", count);
+
+	return (count);
+}
